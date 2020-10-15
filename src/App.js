59af10a4c804f56f3170b8854e3e260e157fb652
Inna Zilberov <innaz@Innas-MBP.fritz.box>
2020-10-15 09:53:40 +0200

    contact list added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import './App.css';
-import Contact from './Contact'
+import ContactList from './components/ContactList'
 
 function App() {
   return (
     <div className="App">
-      <Contact avatar={"https://randomuser.me/api/portraits/women/15.jpg"} 
-      name={"Myrtle Burke"} 
-      online={false}/>
-      <Contact avatar={"https://randomuser.me/api/portraits/women/38.jpg"} 
-      name={"Arlene Rivera"} 
-      online={true}/>
-      <Contact avatar={"https://randomuser.me/api/portraits/men/15.jpg"} 
-      name={"Tommy Hanson"} 
-      online={false}/>
-      
+      <ContactList />
     </div>
   );
 }
